8b343670fe95c9a4a57d3fd6d3bd78f48f5db69c
GrinnRobot <egor.burtovoy4444@gmail.com>
2022-05-23 09:06:36 +0500

    На проверку

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -9,8 +9,8 @@ router = SimpleRouter()
 router.register('posts', PostViewSet)
 router.register('groups', GroupViewSet)
 router.register('follow', FollowViewSet, basename='follow')
-router.register(r'posts/(?P<post_id>\d+)/comments',
-    CommentViewSet, basename='comments')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
