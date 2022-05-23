4880e9441c47e609c2383bce7beaa2a7b07db827
GrinnRobot <egor.burtovoy4444@gmail.com>
2022-05-23 08:53:51 +0500

    На проверку

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,17 +1,17 @@
-from tkinter.tix import Tree
-from tokenize import group
 from django.contrib.auth import get_user_model
 from django.db import models
 
 User = get_user_model()
+
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
-    def __str__(self) :
+    def __str__(self):
         return self.title
+
 
 class Post(models.Model):
     text = models.TextField()
